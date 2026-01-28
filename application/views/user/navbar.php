f060657f50642029709a1486a85d00e2bf3b662d
Angad Avhad <angadavhad069@gmail.com>
2026-01-28 11:29:32 +0530

    push
    pull

--- application/views/user/navbar.php
+++ application/views/user/navbar.php
@@ -12,7 +12,8 @@
   <script src="<?php echo base_url('assets/js/about.js'); ?>" defer></script>
   <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65" crossorigin="anonymous">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
-<!-- Google Font -->
+
+
 <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@300;400;500;600;700&display=swap" rel="stylesheet">
 <head>
 <body>
@@ -26,7 +27,7 @@
 
     <!-- Logo -->
 <div class="ms-logo">
-  <span>Maha</span>Solar
+  <span>convex</span>
 </div>
 
 <!-- Menu -->
