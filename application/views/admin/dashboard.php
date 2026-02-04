71856cb4f65d432fee6f19a6d89f183b0c8f3296
pavangange1-crypto <pavangange1@gmail.com>
2026-02-04 14:38:12 +0530

    admin_pannel_change
    done

---
+++ application/views/admin/dashboard.php
@@ -0,0 +1,29 @@
+
+
+<main class="content">
+  <div class="container-fluid p-4">
+
+    <h1 class="h3 mb-3">Dashboard</h1>
+
+    <div class="card">
+      <div class="card-body">
+        <h4>Solar Energy Project</h4>
+        <p>
+          This dashboard is part of a Solar Energy Management System.
+          It is used to monitor and manage information related to solar energy,
+          blogs, and user contact details.
+        </p>
+
+        <ul>
+          <li>Clean and responsive UI</li>
+          <li>Blog & Contact pages integrated</li>
+          <li>Frontend completed using Bootstrap 5</li>
+          <li>Backend will be developed using PHP & MySQL</li>
+        </ul>
+      </div>
+    </div>
+
+  </div>
+</main>
+
+
