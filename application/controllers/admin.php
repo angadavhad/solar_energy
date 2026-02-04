d9d0571cc31deb14c76019314be7a295cab581ee
pavangange1-crypto <pavangange1@gmail.com>
2026-02-04 11:51:06 +0530

    admin pannel create
    done

---
+++ application/controllers/admin.php
@@ -0,0 +1,24 @@
+<?php
+class Admin extends CI_Controller 
+{
+      public  function navbar()
+      {
+          $this->load->view('admin/navbar');
+      }
+      public function footer()
+      {
+        $this->load->view('admin/footer');
+      }
+       public function index()
+        {
+            $this->navbar();
+            $this->load->view('admin/dashboard');
+            $this->footer();
+        }
+}
+
+
+
+
+
+?>
